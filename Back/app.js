aa0eff773c78260330afefeaf1129e89b443f847
Remy <remy.dufour.etudiant@gmail.com>
2022-06-05 18:56:16 +0200

    err resolu peut etre

--- Back/app.js
+++ Back/app.js
@@ -2,7 +2,7 @@ const express = require('express'); // Importation d'express
 const mongoose = require('mongoose'); // Importation de Mongoose
 
 // Importation d'helmet pour protéger les vulnérabilités connues d'express 
-const helmet = require('helmet')
+const helmet = require('helmet');
 const cors = require('cors');
 
 const userRoutes = require('./routes/user');// Importation des routes User
@@ -25,7 +25,7 @@ app.use(express.json());
 // contre sécurité CORS puisque de base le site qui utilise l'API doit uniquement  des requêtes vers la même origine que celle ci
 app.use(cors());
 
-app.use(helmet())
+app.use(helmet());
 
 // Chemin statique ajouté à l'application pour fournir les images
 app.use(express.static(path.join(__dirname, "public")));
