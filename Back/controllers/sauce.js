598a9ef5e1f986eb939a04a54c28991f5c30ce32
Remy <remy.dufour.etudiant@gmail.com>
2022-06-06 18:38:10 +0200

    ctrls createSauce avec systeme de like et dislike intégré

--- Back/controllers/sauce.js
+++ Back/controllers/sauce.js
@@ -28,5 +28,33 @@ exports.getOneSauce = (req, res, next) => {
   };
 
 // Créer une sauce
-
+exports.createSauce = (req, res, next) => {
+    // Nous devons donc l'analyser à l'aide de JSON.parse() pour obtenir un objet utilisable.
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+        ...sauceObject, // // L'opérateur spread prend l'intégralité des éléments dans le req.body.sauce
+        // Pour l'URL de l'image utilisons req.protocol pour obtenir le premier segment (dans notre cas 'http' ). 
+        // Nous ajoutons '://' , puis utilisons req.get('host') pour résoudre l'hôte du serveur (ici, 'localhost:3000' ). 
+        // Nous ajoutons finalement '/images/' et le nom de fichier pour compléter notre URL.
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    
+        // On initialise les likes et dislikes de la sauce à 0 et les usersLiked et usersDisliked avec des tableaux vides
+        like: 0,
+        dislike: 0,
+        usersliked: [],
+        usersdisliked: [],
+    });
+    sauce.save()
+    .then(() => {
+        res.status(201).json({
+            message: 'Sauce enregistrée !'
+        });
+    })
+    .catch((error) => {
+        res.status(400).json({
+            error: error
+        });
+    });
+}
 
